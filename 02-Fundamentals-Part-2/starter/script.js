9d4a1c4addbee1a04eb2b42c3828e31b80df9a76
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-07-01 07:40:53 -0600

    data type practice

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,33 @@
+let js='amazing';
+console.log(js);
+
+console.log('Ann');
+console.log(52);
+
+let firstName='Ann';
+console.log(firstName);
+console.log("is absolutely amazing");
+
+console.log(true);
+
+let annIsGreat=true;
+console.log(annIsGreat);
+
+console.log(typeof true);
+console.log(typeof annIsGreat);
+console.log(typeof 23);
+console.log(typeof lastName);
+
+annIsGreat = 'You better believe it.';
+console.log(annIsGreat);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
