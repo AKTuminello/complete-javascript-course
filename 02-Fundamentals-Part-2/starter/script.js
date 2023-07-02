0dc956b3b0fce3403a35d3e7c10f9d09027cb99f
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-07-02 09:22:01 -0600

    Added lessons through 26

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,33 +1,197 @@
-let js='amazing';
-console.log(js);
+// let js='amazing';
+// console.log(js);
 
-console.log('Ann');
-console.log(52);
+// console.log('Ann');
+// console.log(52);
 
-let firstName='Ann';
-console.log(firstName);
-console.log("is absolutely amazing");
+// let firstName='Ann';
+// console.log(firstName);
+// console.log("is absolutely amazing");
 
-console.log(true);
+// console.log(true);
 
-let annIsGreat=true;
-console.log(annIsGreat);
+// let annIsGreat=true;
+// console.log(annIsGreat);
 
-console.log(typeof true);
-console.log(typeof annIsGreat);
-console.log(typeof 23);
-console.log(typeof lastName);
+// console.log(typeof true);
+// console.log(typeof annIsGreat);
+// console.log(typeof 23);
+// console.log(typeof lastName);
 
-annIsGreat = 'You better believe it.';
-console.log(annIsGreat);
+// annIsGreat = 'You better believe it.';
+// console.log(annIsGreat);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 2023;
+// year = 2023;
 
-console.log(year);
-console.log(typeof year);
+// console.log(year);
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+
+// const birthYear = 1970
+// let laterYear = 1960
+// let annAge= laterYear - birthYear;
+// console.log(annAge);
+
+// laterYear = 2060
+// annAge = laterYear - birthYear;
+// console.log(annAge);
+
+// let firstName = "Ann";
+// let lastName = "Tuminello";
+
+// console.log(firstName + " " + lastName);
+
+// const firstName = 'Ann';
+// const lastName = 'Tuminello';
+
+// const job = 'Web developer';
+// const birthYear = 1970;
+// const currentYear = 2023
+
+// const ann = "I'm " + firstName + " " + lastName + " and I'm a "
+//  + " " + (currentYear - birthYear) + " year old " + (job);
+// console.log(ann);
+
+// const annNew = `I'm ${firstName} and I'm a ${currentYear - birthYear} year old ${job}!`;
+// console.log(annNew);
+
+// console.log (`Use
+// backticks
+// instead 
+// of 
+// quotes.
+// seriously.`);
+
+// const age= 18;
+
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//     console.log (`You are old enough to drive. 🚘.`);
+
+// }   else {
+//     const yearsLeft = 18 - age;
+//     console.log (`You are too young.
+// Wait another ${yearsLeft} years.`)
+// };
+
+
+// const birthYear = 2001;
+
+// let century;
+
+//  if (birthYear <= 2000) {
+//     century = 20;
+//  } else {
+//     century = 21;
+//  }
+// console.log (century);
+
+
+//  
+
+// input fields as strings to begin with .
+
+// type conversion 
+
+// const inputYear = `1991`;
+// console.log(Number(inputYear));
+// console.log (Number(inputYear) +18);
+
+// console.log(Number(`Ann`));
+// // this actually means an invalid number
+// console.log(typeof NaN);
+
+// console.log(String(23));
+
+// // type coercion
+
+// console.log(`I am ${23} years old.`);
+
+// //template literals seem to be a type of type coercion. 
+
+// //a minus sign triggers a type coercion but a + sign 
+// // gives a concatenation. 
+
+// console.log('23' * '3' - - '4')
+
+// 5 falsy values: 0, '', undefined, null, and NaN.
+// Everything else is truthy.
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Ann'));
+// console.log(Boolean({}));
+// console.log(Boolean());
+// console.log(Boolean());
+
+
+// const money= (-4);
+// if (money){
+//     console.log('Spend it all.');
+// } else {
+//     console.log(`you should get a job`);
+
+// }
+
+// let height = 0;
+// if (height) {
+//     console.log(`Yay, ${height} is defined!`);
+// }else {
+//     console.log(`Height is undefined!`);
+// }
+// // this is not actually true. the boolean is truthy 
+// // because the boolean reacts to zero, even if zero
+// // is a perfectly reasonable value. 
+
+
+// const age = 19;
+
+// if (age===18) console.log (`You're an adult`);
+
+// === is strict. No type conversion. 
+// ==  does type conversion. A string will be converted to a number
+//when it needs to be 
+
+// Use strict for the most part to avoid bugs. 
+// const favorite = prompt(`What's your favorite number?`)
+
+// const hasDriversLicense= false;
+// const hasGoodVision = true;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+
+// if (shouldDrive){
+//     console.log(`You're able to drive.`);
+
+// } else {
+//     console.log(`You're not able to drive`);
+// }
+// const isTired = false; //C
+
+// const shouldDrive= hasDriversLicense && hasGoodVision && isTired;
+
+// console.log(hasDriversLicense && hasGoodVision && !isTired);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+
+// if (shouldDrive){
+//     console.log(`You're able to drive.`);
+
+// } else {
+//     console.log(`You're not able to drive`);
+// }
+
+
+
+
